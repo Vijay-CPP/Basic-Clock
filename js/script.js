736ba4d7c7901c8018c3ac9aff7b3fa6d86a4e41
T Vijay Kumar <74971365+Vijay-CPP@users.noreply.github.com>
2022-04-17 13:50:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function change(num) {
+    if (num < 10)
+        num = "0" + num
+    return num
+}
+
+
+t = document.getElementById("time")
+dt = document.getElementById("date")
+
+let dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"]
+let monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+setInterval(() => {
+    let time = new Date()
+    hrs = time.getHours()
+    let notation = " AM"
+
+    if (hrs > 12)
+        hrs = hrs - 12;
+
+    if (hrs >= 12)
+        notation = " PM"
+    secs = time.getSeconds()
+
+
+    mins = time.getMinutes()
+    secs = time.getSeconds()
+
+    hrs = change(hrs)
+    mins = change(mins)
+    secs = change(secs)
+
+
+    t.innerText = hrs + " : " + mins + " : " + secs + notation;
+
+
+    d = time.getDate()
+    day = dayArray[time.getDay()]
+    month = monthArray[time.getMonth()]
+    year = time.getFullYear()
+
+    dt.innerText = d + " " + month + " , " + year + " - " + day
+
+}, 100)
